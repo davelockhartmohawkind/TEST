cf7e8ddb3dddbd3f18215cdb6b6e46c8f417bfd3
davelockhartmohawkind <dave_lockhart@mohawkind.com>
2013-08-22 15:18:14 -0400

    test2

--- www/includes/scripts/functions.js
+++ www/includes/scripts/functions.js
@@ -30,6 +30,27 @@ $(document).ready(function () {
     // supports_html5_storage();
 
 
+                  $(document).on("click", ".show-page-loading-msg", function () {
+                      var $this = $(this),
+                          theme = $this.jqmData("theme") || $.mobile.loader.prototype.options.theme,
+                          msgText = $this.jqmData("msgtext") || $.mobile.loader.prototype.options.text,
+                          textVisible = $this.jqmData("textvisible") || $.mobile.loader.prototype.options.textVisible,
+                          textonly = !!$this.jqmData("textonly");
+                      html = $this.jqmData("html") || "";
+                      $.mobile.loading("show", {
+                          text: msgText,
+                          textVisible: textVisible,
+                          theme: theme,
+                          textonly: textonly,
+                          html: html
+                      });
+                  })
+
+                  .on("click", ".hide-page-loading-msg", function () {
+                      $.mobile.loading("hide");
+                  });
+
+
 
     $('#profile_name').html(target_profile);
     $('#profile_default').html(target_profile);
